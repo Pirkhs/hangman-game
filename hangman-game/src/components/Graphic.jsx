f8ef4d2165e3efa15b5e91b54417d173994bb08c
Pirkhs <nathan.briers32@gmail.com>
2024-05-21 00:21:35 +0100

    images now uploaded

--- hangman-game/src/components/Graphic.jsx
+++ hangman-game/src/components/Graphic.jsx
@@ -1,8 +1,21 @@
 
 const Graphic = ({gameState}) => {
+    const imageMapper = {
+        1: "df21J8Q",
+        2: "jwQqn1b",
+        3: "7kH2z94",
+        4: "sVCSx9w",
+        5: "F5krxJt",
+        6: "h1HRGLS",
+        7: "sbLQZNC",
+        8: "ZLLT6Lm",
+        9: "7y63yJJ",
+        10: "r4LRHxc",
+    }
+
     return (
     <div className="graphic-section">
-        <img className="graphic" src={`/src/game-state-imgs/game-state-${gameState}.png`}/>
+        <img className="graphic" src={`https://i.ibb.co/${imageMapper[gameState]}/game-state-${gameState}.png`}/>
     </div>
     )
 }
